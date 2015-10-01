7dd6e572b56d0478e1c9cd380ae4b94a5e8190f6
jsa272 <JSA272@nyu.edu>
2015-10-01 19:41:00 -0400

    First commit

---
+++ nyc_project_work.R
@@ -0,0 +1,17 @@
+# Download PLUTO files if it doesn't already exist
+if (!file.exists("data.zip")){
+        download.file("http://www.nyc.gov/html/dcp/download/bytes/nyc_pluto_15v1.zip", "data.zip")
+        unzip("data.zip")
+}
+
+# Import borough data from csv
+mn <- read.csv("mn.csv")
+qn <- read.csv("qn.csv")
+bk <- read.csv("bk.csv")
+bx <- read.csv("bx.csv")
+si <- read.csv("si.csv")
+# Join borough data into single table
+data <- rbind(mn, qn, bk, bx, si)
+rm(mn, qn, bk, bx, si)
+
+
